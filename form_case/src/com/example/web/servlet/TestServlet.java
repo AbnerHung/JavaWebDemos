8023934b11e652c7ac866a20ae0afda4468114b8
めぐみん <59683877+AbnerHung@users.noreply.github.com>
2021-10-14 21:51:46 +0800

    add testServlet

---
+++ form_case/src/com/example/web/servlet/TestServlet.java
@@ -0,0 +1,30 @@
+package com.example.web.servlet;
+
+import com.example.domain.User;
+import com.example.service.UserService;
+import com.example.service.impl.UserServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(value = "/testServlet")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String msg = request.getParameter("msg");
+        System.out.println(
+                name + ": " + msg
+        );
+    }
+}
