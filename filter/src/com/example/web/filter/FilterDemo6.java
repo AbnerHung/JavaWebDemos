07579f7afc3a5d024791a9cc199948974621d5e4
めぐみん <59683877+AbnerHung@users.noreply.github.com>
2021-10-14 23:04:09 +0800

    Add FilterDemo6.java FilterDemo7.java

---
+++ filter/src/com/example/web/filter/FilterDemo6.java
@@ -0,0 +1,22 @@
+package com.example.web.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+//@WebFilter("/*")
+public class FilterDemo6 implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("Filter Demo 6 执行了....");
+        chain.doFilter(request, response);
+        System.out.println("Filter Demo 6 回来了....");
+    }
+}
